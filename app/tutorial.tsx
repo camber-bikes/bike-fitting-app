76724dbca8ceb90ef818558c5c8f7edb388ad547
MarcEgliP <egli@puzzle.ch>
2024-10-08 20:42:22 +0200

    Implement routing

---
+++ app/tutorial.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text} from 'react-native-paper';
+import {StyleSheet} from 'react-native';
+
+const Tutorial = () => {
+    const styles = StyleSheet.create({
+    });
+
+    return (
+        <Text>Hello Tut</Text>
+    )
+}
+
+export default Tutorial;
