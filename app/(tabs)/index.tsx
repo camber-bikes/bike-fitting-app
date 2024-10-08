c8c78b3b95cc6285e3b2d25829c2bb01eac901b1
MarcEgliP <egli@puzzle.ch>
2024-10-08 20:42:22 +0200

    Implement routing

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,33 +1,67 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-
-import HomePage from '../../components/HomePage';
+import { Button, Card, Text} from 'react-native-paper';
+import {StyleSheet} from "react-native";
+import React from "react";
+import {useNavigation} from "@react-navigation/native";
+import {Link} from "expo-router";
+import {Pressable} from "expo-router/build/views/Pressable";
 
 export default function HomeScreen() {
-  return (
-      <HomePage/>
-  );
-}
+        const navigation = useNavigation();
 
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+        const styles = StyleSheet.create({
+                title: {
+                        textAlign: 'center',  // Centers the text horizontally
+                        fontSize: 20,         // You can adjust the font size
+                        fontWeight: 'bold',   // Optional: makes the title bold
+                        marginBottom: "5%",
+                        marginTop: "5%"
+                },
+                contentAlignment: {
+                        width: "100%",
+                        flexDirection: "column",
+                        justifyContent: "center",
+                        textAlign: "center"
+                },
+                homeImage: {
+                        borderRadius: 15,
+                        width: "75%",
+                },
+                actionContainer: {
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "center",
+                        alignItems: "center"
+                },
+                recordingButton: {
+                        width: 60,
+                        height: 60,
+                        marginTop: "5%",
+                        backgroundColor: "red",
+                        borderRadius: 30,
+                        borderWidth: 4,           // Border width of 2 pixels
+                        borderColor: 'black'
+                },
+                actionAdvice: {
+                        fontWeight: "bold"
+                }
+        });
+
+        return (
+            <Card style={{width: "100%", height: "100%", display: "flex", justifyContent: "center"}}>
+                    <Text styile={styles.title}>Camber Bikes</Text>
+                    <Card.Content>
+                            <div style={styles.contentAlignment}>
+                                    <img style={styles.homeImage} src="../assets/images/AngertyBike.jpg" />
+                                    <div style={styles.actionContainer}>
+                                            <Link href="/tutorial" asChild>
+                                                    <Pressable>
+                                                            <Button style={styles.recordingButton}></Button>
+                                                    </Pressable>
+                                            </Link>
+                                            <p style={styles.actionAdvice}>Start recording</p>
+                                    </div>
+                            </div>
+                    </Card.Content>
+            </Card>
+        );
+}
