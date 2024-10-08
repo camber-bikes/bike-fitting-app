3adae80698d4ce9e6a4cf6d6f420ad49e1db8aa5
MarcEgliP <egli@puzzle.ch>
2024-10-08 21:18:49 +0200

    Resolve bugs after merge

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,11 @@
 import { Button, Card, Text} from 'react-native-paper';
 import {StyleSheet} from "react-native";
 import React from "react";
-import {useNavigation} from "@react-navigation/native";
 import {Link} from "expo-router";
 import {Pressable} from "expo-router/build/views/Pressable";
 
 export default function HomeScreen() {
+<<<<<<< HEAD
         const navigation = useNavigation();
 
       BASE_URL = 'http://localhost:8000';
@@ -48,16 +48,18 @@ export default function HomeScreen() {
                 }
         });
 
+=======
+>>>>>>> 7a54318 (Resolve bugs after merge)
         return (
             <Card style={{width: "100%", height: "100%", display: "flex", justifyContent: "center"}}>
-                    <Text styile={styles.title}>Camber Bikes</Text>
+                    <Text style={styles.title}>Camber Bikes</Text>
                     <Card.Content>
                             <div style={styles.contentAlignment}>
                                     <img style={styles.homeImage} src="../assets/images/AngertyBike.jpg" />
                                     <div style={styles.actionContainer}>
                                             <Link href="/tutorial" asChild>
                                                     <Pressable>
-                                                            <Button style={styles.recordingButton}></Button>
+                                                            <Button style={styles.recordingButton} children={undefined}></Button>
                                                     </Pressable>
                                             </Link>
                                             <p style={styles.actionAdvice}>Start recording</p>
@@ -67,3 +69,41 @@ export default function HomeScreen() {
             </Card>
         );
 }
+
+const styles = StyleSheet.create({
+        title: {
+                textAlign: 'center',  // Centers the text horizontally
+                fontSize: 20,         // You can adjust the font size
+                fontWeight: 'bold',   // Optional: makes the title bold
+                marginBottom: "5%",
+                marginTop: "5%"
+        },
+        contentAlignment: {
+                width: "100%",
+                flexDirection: "column",
+                justifyContent: "center",
+                textAlign: "center"
+        },
+        homeImage: {
+                borderRadius: 15,
+                width: "75%",
+        },
+        actionContainer: {
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center"
+        },
+        recordingButton: {
+                width: 60,
+                height: 60,
+                marginTop: "5%",
+                backgroundColor: "red",
+                borderRadius: 30,
+                borderWidth: 4,           // Border width of 2 pixels
+                borderColor: 'black'
+        },
+        actionAdvice: {
+                fontWeight: "bold"
+        }
+});
