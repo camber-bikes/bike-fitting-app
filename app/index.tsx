e7c309e242df3e8b106151dad1f408753a80f5ca
lionelm <lionel.mueller@quickline.ch>
2024-10-10 00:35:32 +0200

    fix: renames

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import { Button, Card, Text} from 'react-native-paper';
-import {StyleSheet} from "react-native";
+import {StyleSheet, ScrollView} from "react-native";
 import React from "react";
 import {Pressable} from "expo-router/build/views/Pressable";
 import { useNavigation } from '@react-navigation/native';
@@ -10,6 +10,7 @@ export default function HomeScreen() {
         const navigation = useNavigation();
 
         return (
+            <ScrollView>
             <Card style={{width: "100%", height: "100%", display: "flex", justifyContent: "center"}}>
                     <Text style={styles.title}>Camber Bikes</Text>
                     <Card.Content>
@@ -24,6 +25,7 @@ export default function HomeScreen() {
                             </div>
                     </Card.Content>
             </Card>
+            </ScrollView>
         );
 }
 
--- app/results.tsx
+++ app/results.tsx
@@ -7,7 +7,7 @@ import { ExternalLink } from '@/components/ExternalLink';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
-import InstructionCard from '../../components/Results/InstructionCard'
+import InstructionCard from '../components/Results/InstructionCard'
 import { useVideoPlayer, VideoView } from 'expo-video';
 
 
